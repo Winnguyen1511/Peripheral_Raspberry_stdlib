0db8b40e738229de610482610e535863b5c8c6f0
Winnguyen <khoanguyen1507dn@gmail.com>
2020-02-28 18:32:14 +0700

    Init repos

--- gitcmd.sh
+++ gitcmd.sh
@@ -2,10 +2,10 @@
 #Git cmd to create repository
 user="Winnguyen1511"
 api=https://api.github.com/user/repos
-reponame="Peripheral Raspberry stdlib"
+reponame="Peripheral_Raspberry_stdlib"
 private="false"
 gitdir=git@github.com:$user/$reponame.git
-curl -u $user $api -d '{"name":"Peripheral Raspberry stdlib"}'
+curl -u $user $api -d '{"name":"Peripheral_Raspberry_stdlib"}'
 
 git init
 git add .
